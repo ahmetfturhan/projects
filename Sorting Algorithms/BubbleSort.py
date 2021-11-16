7b3f5f77a9815826d0749ec9ebd70b08e1457083
Ahmet F. Turhan <afaturhan@gmail.com>
2021-11-17 00:41:18 +0300

    Added Bubble Sort

---
+++ Sorting Algorithms/BubbleSort.py
@@ -0,0 +1,14 @@
+#Author: Ahmet Faruk Turhan
+#Date: 17.11.2021
+#For the sake of practice
+
+a = [8, 5, 6, 2, 0, 9, 3, 1, 7, 4]
+print("Before Bubble Sort:", a)
+
+for i in range(0, len(a)):
+    for j in range(len(a)-1, i, -1):
+        if a[j] < a[j - 1]:
+            a[j], a[j - 1] = a[j - 1], a[j]
+        j -= 1
+
+print("After Bubble Sort:", a) #print the array
